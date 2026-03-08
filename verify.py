d2abff9d10c0377103b24b663afa62c48528ac3f
Lucia <lucia@example.com>
2026-03-08 18:24:40 -0500

    chore: remove competitor comparisons, old scripts, and unused sitemaps

--- verify.py
+++
@@ -1,5 +0,0 @@
-html = open('index.html', encoding='utf-8').read()
-import re
-print('Swiper Wrapper matches:', len(re.findall(r'<div class="swiper-wrapper">', html)))
-print('Swiper Slides matches:', len(re.findall(r'<div class="swiper-slide">', html)))
-print('Services images matches:', len(re.findall(r'/images/services/', html)))
